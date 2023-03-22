a2fbb5cf40a500142e1b86a5392baf1bd4c4a605
Kyaw Kyaw Lwin <kyawkyawlwinthant@gmail.com>
2023-03-22 10:22:06 +0630

    mock

---
+++ module05-question10/src/main/java/com/spring/professional/exam/tutorial/module05/question10/controller/Example12RequestPart.java
@@ -0,0 +1,8 @@
+package com.spring.professional.exam.tutorial.module05.question10.controller;
+
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class Example12RequestPart {
+}
---
+++ module05-question10/src/main/java/com/spring/professional/exam/tutorial/module05/question10/util/InMemoryStorage.java
@@ -0,0 +1,13 @@
+package com.spring.professional.exam.tutorial.module05.question10.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class InMemoryStorage {
+
+    private Map<String,byte[]> contentByName=
+            new HashMap<>();
+
+
+
+}
